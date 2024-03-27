91e13604fbb795543558510063b23b077bd27d5d
issam <issambenslimane9999@hotmail.com>
2024-03-27 22:15:43 +0000

    Add Bencode encoder

---
+++ src/main/java/Bencode/Bencoder.java
@@ -0,0 +1,90 @@
+package Bencode;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.*;
+
+public class Bencoder {
+    private ArrayList<Byte> encoded = new ArrayList<>();
+
+    public Bencoder(Object obj, OutputStream ou) {
+        encode(obj);
+        try{
+            ou.write(getEncoded());
+            ou.close();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Bencoder(Object obj) {
+        encode(obj);
+    }
+
+    public byte[] getEncoded() {
+        byte[] bytes = new byte[encoded.size()];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = encoded.get(i);
+        }
+        return bytes;
+    }
+
+    private void encode(Object obj){
+        try {
+            if (obj instanceof String) writeString((String) obj);
+            else if(obj instanceof byte[]) writeStringBytes((byte[]) obj);
+            else if (obj instanceof Integer) writeInteger((int) obj);
+            else if (obj instanceof List) writeList((List<?>) obj);
+            else if (obj instanceof Map) writeDictionary((Map<String, ?>) obj);
+            else throw new RuntimeException("Unsupported Type.");
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void writeString(String str) throws IOException {
+        writeStringBytes(str.getBytes());
+    }
+
+    private void writeStringBytes(byte[] bytes) throws IOException {
+        int len = bytes.length;
+        String s = String.valueOf(len);
+        for (int i = 0; i < s.length(); i++) {
+            encoded.add((byte) s.charAt(i));
+        }
+        encoded.add((byte) ':');
+        for (byte b: bytes){
+            encoded.add(b);
+        }
+    }
+
+    private void writeInteger(int n) throws IOException {
+        encoded.add((byte) 'i');
+        String s = String.valueOf(n);
+        for (int i = 0; i < s.length(); i++) {
+            encoded.add((byte) s.charAt(i));
+        }
+        encoded.add((byte) 'e');
+    }
+
+    private void writeList(List<?> obj) throws IOException {
+        encoded.add((byte) 'l');
+        for (Object o: obj){
+            encode(o);
+        }
+        encoded.add((byte) 'e');
+    }
+
+    private void writeDictionary(Map<String, ?> obj) throws IOException {
+        Map<String, ?> orderedMap = new TreeMap<>(obj);
+        encoded.add((byte) 'd');
+        for (Map.Entry<String, ?> entry : orderedMap.entrySet()) {
+            String k = entry.getKey();
+            Object v = entry.getValue();
+            encode(k);
+            encode(v);
+        }
+        encoded.add((byte) 'e');
+    }
+}
